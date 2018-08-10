ab74301fd5d974ca653f4bdd9b01cb7422348151
mephi007 <roysumit949@gmail.com>
2018-08-10 11:11:51 +0530

    deciding who is going to take a first move

--- tic-tac.py
+++ tic-tac.py
@@ -1,4 +1,4 @@
-
+import random #for toss
 board=['','','','','','','','','','']
 def drawboard(board):
 	print(' |   |')
@@ -19,13 +19,26 @@ drawboard(board)
 def input_player():
 	letter = ''
 	#while loop - if user mistakenly gives input other than X or O
-	while not(letter == ' X' or letter ==' O'):
+	while not(letter == 'X' or letter =='O'):
 		print('Do you want to be X or O?')
 		letter = raw_input().upper()
 
-	if letter == ' X':
+	if letter == 'X':
 		return ['X' , 'O']
 	else:
 		return['O', 'X']
 
-input_player()
+print(input_player())
+
+def first_move():
+	
+	
+	print('choose number for toss, 0 or 1?')
+	toss = raw_input()
+
+	if random.randint(0,1) == toss:
+		return ' player'
+	else:
+		return 'computer'
+
+print(first_move())
