9a4f86e1959f5f5b820cc0919cef5bb3ce470b57
mephi007 <roysumit949@gmail.com>
2018-08-10 10:55:30 +0530

    taking user input

--- tic-tac.py
+++ tic-tac.py
@@ -10,7 +10,22 @@ def drawboard(board):
 	print(' '+board[6]+ '|' +'   '+ board[5]+ '|' + board[6])
 	print('-------------')
 	print(' '+board[3]+ '|' +'   '+ board[2]+ '|' + board[1])
-
+	print(' |   |')
 
 drawboard(board)
 
+#taking input from user
+
+def input_player():
+	letter = ''
+	#while loop - if user mistakenly gives input other than X or O
+	while not(letter == ' X' or letter ==' O'):
+		print('Do you want to be X or O?')
+		letter = raw_input().upper()
+
+	if letter == ' X':
+		return ['X' , 'O']
+	else:
+		return['O', 'X']
+
+input_player()
